aca995031ee9b24f9be2c7b5c61ba4a65b1ade72
Andrew Dunham <andrew@du.nham.ca>
2014-07-07 14:22:53 -0700

    Fix package name and tests

--- graceful_test.go
+++ graceful_test.go
@@ -5,155 +5,128 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"os"
 	"sync"
 	"syscall"
 	"testing"
 	"time"
 )
 
-var killTime = 50 * time.Millisecond
+const KILL_TIME = 50 * time.Millisecond
 
 func runQuery(t *testing.T, expected int, shouldErr bool, wg *sync.WaitGroup) {
 	wg.Add(1)
 	defer wg.Done()
+
 	client := http.Client{}
 	r, err := client.Get("http://localhost:3000")
-	if shouldErr && err == nil {
-		t.Fatal("Expected an error but none was encountered.")
-	} else if shouldErr && err != nil {
-		if err.(*url.Error).Err == io.EOF {
-			return
-		}
-		errno := err.(*url.Error).Err.(*net.OpError).Err.(syscall.Errno)
-		if errno == syscall.ECONNREFUSED {
-			return
-		} else if err != nil {
-			t.Fatal("Error on Get:", err)
+
+	if shouldErr {
+		if err == nil {
+			t.Fatal("Expected an error but none was encountered")
+		} else {
+			if err.(*url.Error).Err == io.EOF {
+				return
+			}
+
+			errno := err.(*url.Error).Err.(*net.OpError).Err.(syscall.Errno)
+			if errno == syscall.ECONNREFUSED {
+				return
+			} else if err != nil {
+				t.Fatal("Error on Get:", err)
+			}
 		}
 	}
 
 	if r != nil && r.StatusCode != expected {
-		t.Fatalf("Incorrect status code on response. Expected %d. Got %d", expected, r.StatusCode)
+		t.Fatalf("Incorrect status code on response: (expected) %d != %d (actual)",
+			expected, r.StatusCode)
 	} else if r == nil {
-		t.Fatal("No response when a response was expected.")
+		t.Fatal("No response when a response was expected")
 	}
 }
 
-func runServer(timeout, sleep time.Duration, c chan os.Signal) error {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		time.Sleep(sleep)
-		rw.WriteHeader(http.StatusOK)
-	})
-	srv := &http.Server{Addr: ":3000", Handler: mux}
-	l, err := net.Listen("tcp", ":3000")
-	if err != nil {
-		return err
-	}
-	return run(srv, l, timeout, c)
+func runServer(t *testing.T, timeout, sleep time.Duration) (srv *GracefulServer, wg *sync.WaitGroup) {
+	wg = &sync.WaitGroup{}
+	srv = NewServer()
+
+	srv.Timeout = timeout
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+			time.Sleep(sleep)
+			rw.WriteHeader(http.StatusOK)
+		})
+
+		if err := srv.Run(":3000", mux); err != nil {
+			t.Fatal("Error from graceful run: %s", err)
+		}
+	}()
+	return
 }
 
 func TestGracefulRun(t *testing.T) {
-	c := make(chan os.Signal, 1)
+	srv, wg := runServer(t, KILL_TIME, KILL_TIME/2)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	for i := 0; i < 10; i++ {
+		go runQuery(t, http.StatusOK, false, wg)
+	}
 
-	go func() {
-		runServer(killTime, killTime/2, c)
-		wg.Done()
-	}()
+	time.Sleep(10 * time.Millisecond)
+	srv.Shutdown <- struct{}{}
+	time.Sleep(10 * time.Millisecond)
 
-	wg.Add(1)
-	go func() {
-		for i := 0; i < 8; i++ {
-			go runQuery(t, http.StatusOK, false, &wg)
-		}
-		time.Sleep(10 * time.Millisecond)
-		c <- os.Interrupt
-		time.Sleep(10 * time.Millisecond)
-		for i := 0; i < 8; i++ {
-			go runQuery(t, 0, true, &wg)
-		}
-		wg.Done()
-	}()
+	for i := 0; i < 10; i++ {
+		go runQuery(t, 0, true, wg)
+	}
 
 	wg.Wait()
-
 }
 
 func TestGracefulRunTimesOut(t *testing.T) {
-	c := make(chan os.Signal, 1)
+	srv, wg := runServer(t, KILL_TIME, KILL_TIME*10)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	for i := 0; i < 10; i++ {
+		go runQuery(t, 0, true, wg)
+	}
 
-	go func() {
-		runServer(killTime, killTime*10, c)
-		wg.Done()
-	}()
+	time.Sleep(10 * time.Millisecond)
+	srv.Shutdown <- struct{}{}
+	time.Sleep(10 * time.Millisecond)
 
-	wg.Add(1)
-	go func() {
-		for i := 0; i < 8; i++ {
-			go runQuery(t, 0, true, &wg)
-		}
-		time.Sleep(10 * time.Millisecond)
-		c <- os.Interrupt
-		time.Sleep(10 * time.Millisecond)
-		for i := 0; i < 8; i++ {
-			go runQuery(t, 0, true, &wg)
-		}
-		wg.Done()
-	}()
+	for i := 0; i < 10; i++ {
+		go runQuery(t, 0, true, wg)
+	}
 
 	wg.Wait()
-
 }
 
 func TestGracefulRunDoesntTimeOut(t *testing.T) {
-	c := make(chan os.Signal, 1)
+	srv, wg := runServer(t, 0, KILL_TIME*2)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	for i := 0; i < 10; i++ {
+		go runQuery(t, http.StatusOK, false, wg)
+	}
 
-	go func() {
-		runServer(0, killTime*2, c)
-		wg.Done()
-	}()
+	time.Sleep(10 * time.Millisecond)
+	srv.Shutdown <- struct{}{}
+	time.Sleep(10 * time.Millisecond)
 
-	wg.Add(1)
-	go func() {
-		for i := 0; i < 8; i++ {
-			go runQuery(t, http.StatusOK, false, &wg)
-		}
-		time.Sleep(10 * time.Millisecond)
-		c <- os.Interrupt
-		time.Sleep(10 * time.Millisecond)
-		for i := 0; i < 8; i++ {
-			go runQuery(t, 0, true, &wg)
-		}
-		wg.Done()
-	}()
+	for i := 0; i < 10; i++ {
+		go runQuery(t, 0, true, wg)
+	}
 
 	wg.Wait()
-
 }
 
 func TestGracefulRunNoRequests(t *testing.T) {
-	c := make(chan os.Signal, 1)
+	srv, wg := runServer(t, 0, KILL_TIME*2)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		runServer(0, killTime*2, c)
-		wg.Done()
-	}()
-
-	c <- os.Interrupt
+	time.Sleep(10 * time.Millisecond)
+	srv.Shutdown <- struct{}{}
 
 	wg.Wait()
-
 }
