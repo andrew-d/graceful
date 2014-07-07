d08d8a12557dc23ce0686a52f03ff580223d78e2
Andrew Dunham <andrew@du.nham.ca>
2014-07-07 00:03:00 -0700

    New architecture allowing manual shutdown

--- graceful.go
+++ graceful.go
@@ -1,97 +1,62 @@
-package graceful
+package main
 
 import (
-	"crypto/tls"
-	"log"
 	"net"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
-// Run serves the http.Handler with graceful shutdown enabled.
-//
-// timeout is the duration to wait until killing active requests and stopping the server.
-// If timeout is 0, the server never times out. It waits for all active requests to finish.
-func Run(addr string, timeout time.Duration, n http.Handler) {
-	srv := &http.Server{Addr: addr, Handler: n}
-	err := ListenAndServe(srv, timeout)
+type GracefulServer struct {
+	Shutdown chan struct{}
+	Timeout  time.Duration
+}
 
-	if err != nil {
-		if opErr, ok := err.(*net.OpError); !ok || (ok && opErr.Op != "accept") {
-			logger := log.New(os.Stdout, "[graceful] ", 0)
-			logger.Fatal(err)
-		}
+func NewServer() *GracefulServer {
+	return &GracefulServer{
+		Shutdown: make(chan struct{}),
+		Timeout:  10 * time.Second,
 	}
 }
 
-// ListenAndServe is equivalent to http.Server.ListenAndServe with graceful shutdown enabled.
-//
-// timeout is the duration to wait until killing active requests and stopping the server.
-// If timeout is 0, the server never times out. It waits for all active requests to finish.
-func ListenAndServe(srv *http.Server, timeout time.Duration) error {
-	// Create the listener so we can control their lifetime
+// Run serves the given http.Handler with graceful shutdown enabled.
+func (s *GracefulServer) Run(addr string, handler http.Handler) error {
+	// Create a dummy http.Server and call through to ListenAndServe.
+	srv := &http.Server{Addr: addr, Handler: handler}
+	return s.ListenAndServe(srv)
+}
+
+// ListenAndServe is equivalent to http.Server.ListenAndServe with graceful
+// shutdown enabled.
+func (s *GracefulServer) ListenAndServe(srv *http.Server) error {
 	addr := srv.Addr
 	if addr == "" {
 		addr = ":http"
 	}
+
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		return err
 	}
 
-	return run(srv, l, timeout, make(chan os.Signal, 1))
+	return s.Serve(srv, l)
 }
 
-// ListenAndServeTLS is equivalent to http.Server.ListenAndServeTLS with graceful shutdown enabled.
-//
-// timeout is the duration to wait until killing active requests and stopping the server.
-// If timeout is 0, the server never times out. It waits for all active requests to finish.
-func ListenAndServeTLS(srv *http.Server, certFile, keyFile string, timeout time.Duration) error {
-	// Create the listener so we can control their lifetime
-	addr := srv.Addr
-	if addr == "" {
-		addr = ":https"
-	}
-	config := &tls.Config{}
-	if srv.TLSConfig != nil {
-		*config = *srv.TLSConfig
-	}
-	if config.NextProtos == nil {
-		config.NextProtos = []string{"http/1.1"}
-	}
-
-	var err error
-	config.Certificates = make([]tls.Certificate, 1)
-	config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return err
-	}
-
-	conn, err := net.Listen("tcp", addr)
-	if err != nil {
-		return err
-	}
-
-	tlsListener := tls.NewListener(conn, config)
-	return run(srv, tlsListener, timeout, make(chan os.Signal, 1))
+// ListenAndServeTLS is equivalent to http.Server.ListenAndServeTLS with
+// graceful shutdown enabled.
+func (s *GracefulServer) ListenAndServeTLS(srv *http.Server, certFile, keyFile string) error {
+	// TODO: implement me
+	return nil
 }
 
 // Serve is equivalent to http.Server.Serve with graceful shutdown enabled.
 //
-// timeout is the duration to wait until killing active requests and stopping the server.
-// If timeout is 0, the server never times out. It waits for all active requests to finish.
-func Serve(srv *http.Server, l net.Listener, timeout time.Duration) error {
-	return run(srv, l, timeout, make(chan os.Signal, 1))
-}
-
-func run(server *http.Server, listener net.Listener, timeout time.Duration, c chan os.Signal) error {
-	// Track connection state
+// This function will only return when the server has been properly shut down
+// and all open connections have been closed.
+func (s *GracefulServer) Serve(srv *http.Server, listener net.Listener) error {
+	// We need to track the connection state of all inbound connections.
 	add := make(chan net.Conn)
 	remove := make(chan net.Conn)
-	server.ConnState = func(conn net.Conn, state http.ConnState) {
+	srv.ConnState = func(conn net.Conn, state http.ConnState) {
 		switch state {
 		case http.StateActive:
 			add <- conn
@@ -100,27 +65,39 @@ func run(server *http.Server, listener net.Listener, timeout time.Duration, c ch
 		}
 	}
 
-	// Manage open connections
+	// This goroutine manages the open connections.
+	// In short, we do:
+	//	- Track all open connections in a "set" (i.e. map to struct{})
+	//	- Remove closed connections from the map
+	//	- A request to stop consists of a channel, which we then signal when all
+	//	  the connections have been closed.
+	//	- A kill request causes us to forcefully close all remaining open
+	//	  connections
 	stop := make(chan chan struct{})
 	kill := make(chan struct{})
 	go func() {
 		var done chan struct{}
+		var conn net.Conn
+
 		connections := map[net.Conn]struct{}{}
 		for {
 			select {
-			case conn := <-add:
+			case conn = <-add:
 				connections[conn] = struct{}{}
-			case conn := <-remove:
+
+			case conn = <-remove:
 				delete(connections, conn)
 				if done != nil && len(connections) == 0 {
 					done <- struct{}{}
 					return
 				}
+
 			case done = <-stop:
 				if len(connections) == 0 {
 					done <- struct{}{}
 					return
 				}
+
 			case <-kill:
 				for k := range connections {
 					k.Close()
@@ -130,32 +107,53 @@ func run(server *http.Server, listener net.Listener, timeout time.Duration, c ch
 		}
 	}()
 
-	// Set up the interrupt catch
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	// The shutdown channel is a request from the user to stop listening.  The
+	// first time it's signalled, we close the listener and turn off keep-
+	// alives, which causes the Serve() call below to return.
 	go func() {
-		for _ = range c {
-			server.SetKeepAlivesEnabled(false)
-			listener.Close()
-			signal.Stop(c)
-			close(c)
-		}
+		<-s.Shutdown
+		srv.SetKeepAlivesEnabled(false)
+		listener.Close()
 	}()
 
-	// Serve with graceful listener
-	err := server.Serve(listener)
+	// Now, we serve the given server with our new graceful listener.  This
+	// will return when the listener is closed, above.
+	err := srv.Serve(listener)
 
-	// Request done notification
-	done := make(chan struct{})
-	stop <- done
+	// Send the channel above another channelt that it will use to tell us when
+	// everything is finished.
+	finished := make(chan struct{})
+	stop <- finished
 
-	if timeout > 0 {
-		select {
-		case <-done:
-		case <-time.After(timeout):
-			kill <- struct{}{}
-		}
-	} else {
-		<-done
+	// A nil channel will always block, so we leave the timeout channel as nil
+	// if there's a timeout of 0.
+	var timeout <-chan time.Time
+	if s.Timeout > 0 {
+		timeout = time.After(s.Timeout)
 	}
+
+	// Now, if we get a second shutdown request, or if we don't get the 'done'
+	// response before the configured timeout, then we send the kill request to
+	// the server and exit.
+	select {
+	case <-timeout:
+		kill <- struct{}{}
+
+	case <-s.Shutdown:
+		kill <- struct{}{}
+
+	case <-finished:
+		// Do nothing - we're closed.
+	}
+
+	// The error isn't an error if it happened due to failure to accept - this
+	// is expected because we closed the listener.
+	if err != nil {
+		opErr, ok := err.(*net.OpError)
+		if ok && opErr.Op == "accept" {
+			return nil
+		}
+	}
+
 	return err
 }
