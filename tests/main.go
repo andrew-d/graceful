9fc657804bdb4125e0b52e3cfefab9312f7f994b
Andrew Dunham <andrew@du.nham.ca>
2014-07-07 15:22:40 -0700

    Update README, add TravisCI

--- tests/main.go
+++ tests/main.go
@@ -2,39 +2,60 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
 
+	"github.com/andrew-d/graceful"
 	"github.com/codegangsta/negroni"
-	"github.com/stretchr/graceful"
 )
 
 func main() {
-
 	var wg sync.WaitGroup
+
+	srv1 := graceful.NewServer()
+	srv2 := graceful.NewServer()
+	srv3 := graceful.NewServer()
 
 	wg.Add(3)
 	go func() {
 		n := negroni.New()
 		fmt.Println("Launching server on :3000")
-		graceful.Run(":3000", 0, n)
+		srv1.Run(":3000", n)
 		fmt.Println("Terminated server on :3000")
 		wg.Done()
 	}()
+
 	go func() {
 		n := negroni.New()
 		fmt.Println("Launching server on :3001")
-		graceful.Run(":3001", 0, n)
+		srv2.Run(":3001", n)
 		fmt.Println("Terminated server on :3001")
 		wg.Done()
 	}()
+
 	go func() {
 		n := negroni.New()
 		fmt.Println("Launching server on :3002")
-		graceful.Run(":3002", 0, n)
+		srv3.Run(":3002", n)
 		fmt.Println("Terminated server on :3002")
 		wg.Done()
 	}()
-	fmt.Println("Press ctrl+c. All servers should terminate.")
-	wg.Wait()
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	go func(){
+		for _ = range c {
+			srv1.Shutdown <- struct{}{}
+			srv2.Shutdown <- struct{}{}
+			srv3.Shutdown <- struct{}{}
+
+			signal.Stop(c)
+			close(c)
+		}
+	}()
+
+	fmt.Println("Press Ctrl+C. All servers should terminate.")
+	wg.Wait()
+	fmt.Println("Finished!")
 }
